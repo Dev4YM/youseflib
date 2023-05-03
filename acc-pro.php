a971f2e83bdf2ba1c930a584c00ba14e85d129f9
Yousef Mohammed <112990079+youseflib@users.noreply.github.com>
2023-05-04 00:42:22 +0300

    Update acc-pro.php

--- acc-pro.php
+++ acc-pro.php
@@ -1,1 +1,28 @@
+<?php 
 
+    if(isset($_POST['btn-send']))
+    {
+       $UserName = $_POST['UName'];
+       $Email = $_POST['Email'];
+       $Subject = $_POST['Subject'];
+       $Msg = $_POST['msg'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       {
+           header('location:lo.php?error');
+       }
+       else
+       {
+           $to = "yousef6k@gmail.com";
+
+           if(mail($to,$Subject,$Msg,$Email))
+           {
+               header("location:lo.php?success");
+           }
+       }
+    }
+    else
+    {
+        header("location:lo.php");
+    }
+?>
