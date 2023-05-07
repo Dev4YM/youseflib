7d3308dc05bee4ec5476501b4c2a8927fb94ec56
Yousef Mohammed <112990079+youseflib@users.noreply.github.com>
2023-05-08 01:15:47 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function editName() {
+			document.querySelector("#name").readOnly = false;
+			document.querySelector(".container").classList.add("edit-mode");
+            const nameInput = document.querySelector("#name");
+            nameInput.readOnly = false;
+            nameInput.focus();
+            document.querySelector("#edit-btn").style.display = "none";
+		}
+		function updateName() {
+			document.querySelector("#name").readOnly = true;
+			document.querySelector(".container").classList.remove("edit-mode");
+            document.querySelector("#edit-btn").style.display = "block";
+            localStorage.setItem("nameInputValue", nameInput.value);
+        }
+		function cancelEdit() {
+            const nameInput = document.querySelector("#name");
+            nameInput.value = "";
+            nameInput.readOnly = true;
+            document.querySelector(".container").classList.remove("edit-mode");
+            document.querySelector("#edit-btn").style.display = "block";
+        }
